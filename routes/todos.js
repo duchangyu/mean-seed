4027c3278171ea1f7e46a76a9972ab3d8da127af
Daniel Du <duchangyu@gmail.com>
2015-07-23 15:45:41 +0800

    add new todo item

--- routes/todos.js
+++ routes/todos.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var fs = require('fs');
 
+//var sampleToDoItems = JSON.parse(fs.readFileSync('todoTasks.json','utf8'));
+var sampleToDoItems = require('../todoTasks.json');
+
 /* GET Todo listing. */
 //GET /Todos/
 router.get('/', function(req, res) {
@@ -10,15 +13,26 @@ router.get('/', function(req, res) {
   res.type('application/json');
   res.status(200);
 
-  var sampleToDoItems = JSON.parse(fs.readFileSync('todoTasks.json','utf8'));
+  
   res.json(sampleToDoItems);
 });
 
 
 
-//POST /Todos/
-router.get('/', function(req, res) {
-  res.send('add a new todo items ');
+//add new todo items
+//POST /Todos/ 
+router.post('/', function(req, res) {
+    
+  var todoItem = {
+    task : req.body.task, 
+    complete : req.body.complete
+  };
+
+  sampleToDoItems.push(todoItem);
+
+  res.type('application/json');
+  res.status(200);  
+  res.json(sampleToDoItems);
 });
 
 
